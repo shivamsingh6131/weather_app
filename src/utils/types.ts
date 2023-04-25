f087879c3507b66f73ce74f7b41b87f4ce6a6d7e
Neeldip Barot <neeldip.barot@bos.no>
2023-04-25 14:43:38 +0530

    Refactored

---
+++ src/utils/types.ts
@@ -0,0 +1,14 @@
+export interface ICustomTypographyProps {
+  component?: React.Component;
+  condition: boolean;
+  typegraphyData: any;
+  temperatureData?: string;
+  typegraphystyles?: any;
+  additionalProps?: any;
+  loaderHeightWidth?: string;
+}
+
+export interface Icordinates {
+  latitude: number;
+  longitude: number;
+}
